6226b024d730ee21288d1940bf2d7c95d5aba962
alenaartemyeva <a.artemyeva@along.pw>
2025-11-30 06:52:47 +0300

    Fix layout

--- src/components/LayerControls.js
+++ src/components/LayerControls.js
@@ -6,19 +6,18 @@ import TransportIcon from '../assets/transport-sign.svg';import { IoBuildOutline
 
 const LayerControls = ({ layers, onToggle }) => {
   return (
-    <div>
-      <div className="absolute inset-0 flex items-center justify-center z-10">
-        <div className="flex flex-col items-center text-sm font-medium text-black px-3 py-1 rounded shadow-sm">
-        <IoBuildOutline className='w-6 h-6'/>
-        Скоро будет доступно
+    <div className="grow flex flex-col min-h-0">
+      <div className='p-4 md:p-0 md:pt-4 md:border-t border-l md:border-l-0 md:border-gray-200 relative w-full h-fit self-start'>
+        <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-sm font-medium text-black">
+          <IoBuildOutline className='w-6 h-6'/>
+          Скоро будет доступно
+        </div>
+        <div className='flex flex-col gap-4 items-center md:items-start opacity-25 pointer-events-none'>
+          <CustomToggle text="Места для остановки" icon={<CustomIcon icon={StopIcon}/>} id="stops-desktop" checked={layers.stops} onToggle={() => {}} />
+          <CustomToggle text="Разрешенная скорость" icon={<CustomIcon icon={SpeedIcon}/>} id="speed-desktop" checked={layers.speed} onToggle={() => {}} />
+          <CustomToggle text="Остановки транспорта" icon={<CustomIcon icon={TransportIcon}/>} id="transport-desktop" checked={layers.transport} onToggle={() => {}} />
         </div>
       </div>
-    <div className='grow p-4 md:p-0 md:pt-4 md:border-t border-l md:border-l-0 md:border-gray-200 flex flex-col gap-4 items-center md:items-start opacity-25 pointer-events-none relative'>
-      
-      <CustomToggle text="Места для остановки" icon={<CustomIcon icon={StopIcon}/>} id="stops-desktop" checked={layers.stops} onToggle={() => {}} />
-      <CustomToggle text="Разрешенная скорость" icon={<CustomIcon icon={SpeedIcon}/>} id="speed-desktop" checked={layers.speed} onToggle={() => {}} />
-      <CustomToggle text="Остановки транспорта" icon={<CustomIcon icon={TransportIcon}/>} id="transport-desktop" checked={layers.transport} onToggle={() => {}} />
-    </div>
     </div>
   );
 };
